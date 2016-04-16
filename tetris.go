efc88b7534e8a47315e92462288c3119e5bb1c66
Ryan Hoegg <ryan.hoegg@gmail.com>
2016-04-16 16:05:21 -0500

    Add game board

--- tetris.go
+++ tetris.go
@@ -2,8 +2,17 @@ package tetris
 
 type TetrisGame struct {
     difficulty int
+    *Board
+}
+
+type Board struct {
+    width int
 }
 
 func NewTetrisGame() *TetrisGame {
-    return &TetrisGame{difficulty: 1}
+    return &TetrisGame{difficulty: 1, Board: NewTetrisBoard()}
+}
+
+func NewTetrisBoard() *Board {
+    return &Board{width:10}
 }
--- tetris_test.go
+++ tetris_test.go
@@ -13,3 +13,8 @@ func TestInitialDifficulty(t *testing.T) {
     game := NewTetrisGame()
     assert.Equal(t, 1, game.difficulty, "Difficulty should default to 1")
 }
+
+func TestInitialBoardWidth(t *testing.T) {
+    game := NewTetrisGame()
+    assert.Equal(t, 10, game.Board.width, "Width should be 10")
+}
